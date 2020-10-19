7162ad12d38dc960a8e348bb887edd0c59b1eb8b
rhsin <ryanhsin@ymail.com>
2020-10-18 17:38:16 -0700

    new TokensController for simple JWT token response (no user)

---
+++ Controllers/TokensController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MvcTitle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokensController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: api/Tokens
+        [HttpGet, AllowAnonymous]
+        public IActionResult CreateToken()
+        {
+            var tokenString = BuildToken();
+
+            IActionResult response = Ok(new { token = tokenString });
+
+            return response;
+        }
+
+        private string BuildToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"], _config["Jwt:Issuer"], signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
